e1d420405435cf93db810597fea1c2a7aade2d4f
Sam_Osho <Sosho108@marian.edu>
2023-10-19 21:25:12 -0400

    updates

--- servers.py
+++ servers.py
@@ -3,7 +3,16 @@ app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 @app.route('/')          # The "@" decorator associates this route with the function immediately following
 def hello_world():
     return 'Hello World!'  # Return the string 'Hello World!' as a response
+
+@app.route('/success')
+def success():
+    return "success"
+    
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
-    app.run(debug=True)    # Run the app in debug mode.
+    app.run(debug=True)  
 
-
+        
+# @app.route('/success')
+# def success():
+#     return "success"
+    
